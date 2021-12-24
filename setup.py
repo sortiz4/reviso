52428fdee45da736cb88d4f7511b208d507f9c96
Steven Ortiz <sortiz832@gmail.com>
2021-12-24 14:09:32 -0600

    Updated setup

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from subprocess import run
 from urllib import request
 from zipfile import ZipFile
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ICONS_URL = 'https://github.com/sortiz4/reviso/releases/download/1.0/1.0.zip'
-ICONS_DEST = os.path.join(BASE_DIR, 'src/main/resources/reviso/icon')
-ICONS_SIZES = [16, 32, 64, 128, 256]
-WRAPPER_SRC = os.path.join(BASE_DIR, 'src/main/rust/reviso.rs')
-WRAPPER_DEST = os.path.join(BASE_DIR, 'src/dist/bin')
-WRAPPER_SETUP = ['rustc', WRAPPER_SRC, '--out-dir', WRAPPER_DEST, '-C', 'opt-level=3']
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+ICONS_SRC_URL = 'https://github.com/sortiz4/reviso/releases/download/1.0/1.0.zip'
+ICONS_SRC_SIZES = [16, 32, 64, 128, 256]
+ICONS_DEST_PATH = os.path.join(BASE_PATH, 'src/main/resources/reviso/icon')
+WRAPPER_SRC_PATH = os.path.join(BASE_PATH, 'src/main/rust/reviso.rs')
+WRAPPER_DEST_PATH = os.path.join(BASE_PATH, 'src/dist/bin')
+WRAPPER_SETUP_TASK = ['rustc', WRAPPER_SRC_PATH, '--out-dir', WRAPPER_DEST_PATH, '-C', 'opt-level=3']
 
 
 class Command:
@@ -59,26 +59,26 @@ class Command:
                 return '{}.png'.format(icon_size)
 
             # Stop if one of the icons exist
-            if os.path.exists(os.path.join(ICONS_DEST, get_icon_name(ICONS_SIZES[0]))):
+            if os.path.exists(os.path.join(ICONS_DEST_PATH, get_icon_name(ICONS_SRC_SIZES[0]))):
                 return
 
             # Make the directories if they don't exist
-            if not os.path.exists(ICONS_DEST):
-                os.makedirs(ICONS_DEST)
+            if not os.path.exists(ICONS_DEST_PATH):
+                os.makedirs(ICONS_DEST_PATH)
 
             # Download the icon archive
-            archive = ZipFile(BytesIO(request.urlopen(ICONS_URL).read()))
+            archive = ZipFile(BytesIO(request.urlopen(ICONS_SRC_URL).read()))
 
             # Extract the icons
-            for size in ICONS_SIZES:
+            for size in ICONS_SRC_SIZES:
                 name = get_icon_name(size)
                 content = archive.read('icons/png/{}'.format(name))
-                with open(os.path.join(ICONS_DEST, name), 'wb') as icon:
+                with open(os.path.join(ICONS_DEST_PATH, name), 'wb') as icon:
                     icon.write(content)
 
         def wrapper():
             # Compile the wrapper
-            run(WRAPPER_SETUP)
+            run(WRAPPER_SETUP_TASK)
 
         if self.args.icons:
             icons()
